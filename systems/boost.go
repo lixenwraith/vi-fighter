75fbd9d8fc06c3ecb13eb661127ec1f6b96ed936
Lixen Wraith <lixen.wraith@gmail.com>
2025-11-28 04:37:52 -0500

    ea:p1i4-fp2i2 feat:shield visual and other blending stuff, god my renderer sucks

--- systems/boost.go
+++ systems/boost.go
@@ -3,8 +3,10 @@ package systems
 import (
 	"time"
 
+	"github.com/lixenwraith/vi-fighter/components"
 	"github.com/lixenwraith/vi-fighter/constants"
 	"github.com/lixenwraith/vi-fighter/engine"
+	"github.com/lixenwraith/vi-fighter/render"
 )
 
 type BoostSystem struct {
@@ -20,12 +22,37 @@ func (bs *BoostSystem) Priority() int {
 	return constants.PriorityBoost
 }
 
+// Update handles status bar timer update and shield component lifecycle anchored on cursor
 func (bs *BoostSystem) Update(world *engine.World, dt time.Duration) {
 	// Fetch resources
 	timeRes := engine.MustGetResource[*engine.TimeResource](world.Resources)
 	now := timeRes.GameTime
 
-	if bs.ctx.State.UpdateBoostTimerAtomic(now) {
-		// Boost expired
+	// Update timer state
+	bs.ctx.State.UpdateBoostTimerAtomic(now)
+
+	// Sync Shield component with Boost state
+	boostEnabled := bs.ctx.State.GetBoostEnabled()
+	cursorEntity := bs.ctx.CursorEntity
+
+	hasShield := world.Shields.Has(cursorEntity)
+
+	if boostEnabled {
+		if !hasShield {
+			// Create shield component
+			shield := components.ShieldComponent{
+				Active:     true,
+				RadiusX:    constants.ShieldRadiusX,
+				RadiusY:    constants.ShieldRadiusY,
+				Color:      render.RgbShieldBase,
+				MaxOpacity: constants.ShieldMaxOpacity,
+			}
+			world.Shields.Add(cursorEntity, shield)
+		}
+	} else {
+		if hasShield {
+			// Remove shield when boost is disabled
+			world.Shields.Remove(cursorEntity)
+		}
 	}
 }
