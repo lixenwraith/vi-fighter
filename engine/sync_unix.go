1c99d61007914813af6caf689e66b6aea1ccda45
Lixen Wraith <lixen.wraith@gmail.com>
2026-02-16 05:56:27 -0500

    opt: wasm-related tweaks. runs faster in chrome, webgl2 is a must for decent frame rates. there is some character corruption with webgl that gets cleaned up.

--- engine/sync_unix.go
+++ engine/sync_unix.go
@@ -4,13 +4,21 @@ package engine
 
 import "sync"
 
-// WorldMutex is real mutex on Unix
+// WorldMutex wraps sync.RWMutex for entity/system registration
 type WorldMutex struct {
 	mu sync.RWMutex
 }
 
-func (m *WorldMutex) Lock()         { m.mu.Lock() }
-func (m *WorldMutex) Unlock()       { m.mu.Unlock() }
-func (m *WorldMutex) RLock()        { m.mu.RLock() }
-func (m *WorldMutex) RUnlock()      { m.mu.RUnlock() }
-func (m *WorldMutex) TryLock() bool { return m.mu.TryLock() }
+func (m *WorldMutex) Lock()    { m.mu.Lock() }
+func (m *WorldMutex) Unlock()  { m.mu.Unlock() }
+func (m *WorldMutex) RLock()   { m.mu.RLock() }
+func (m *WorldMutex) RUnlock() { m.mu.RUnlock() }
+
+// UpdateMutex wraps sync.Mutex for game tick serialization
+type UpdateMutex struct {
+	mu sync.Mutex
+}
+
+func (m *UpdateMutex) Lock()         { m.mu.Lock() }
+func (m *UpdateMutex) Unlock()       { m.mu.Unlock() }
+func (m *UpdateMutex) TryLock() bool { return m.mu.TryLock() }
